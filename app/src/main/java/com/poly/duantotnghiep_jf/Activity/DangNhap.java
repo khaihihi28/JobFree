f6e46f46ae1ab03d2a1ac4a1c58a951e0bd12244
Administrator <sonbhph24050@gmail.com>
2023-10-05 16:15:16 +0700

    2 màn hình đăng nhập và quên mật khẩu

---
+++ app/src/main/java/com/poly/duantotnghiep_jf/Activity/DangNhap.java
@@ -0,0 +1,20 @@
+package com.poly.duantotnghiep_jf.Activity;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+
+import com.poly.duantotnghiep_jf.R;
+
+public class DangNhap extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_dang_nhap);
+
+    }
+}
