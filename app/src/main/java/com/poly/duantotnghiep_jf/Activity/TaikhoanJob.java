fe6a81cae65e2739826c875d3d0a9d89ea430246
khaihihi28 <khaihihi28@gmail.com>
2023-10-11 20:19:13 +0700

    Dang ky dang nhap basic

--- app/src/main/java/com/poly/duantotnghiep_jf/Activity/TaikhoanJob.java
+++ app/src/main/java/com/poly/duantotnghiep_jf/Activity/TaikhoanJob.java
@@ -1,24 +1,66 @@
 package com.poly.duantotnghiep_jf.Activity;
 
+import static android.content.ContentValues.TAG;
+
+import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.content.Intent;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.View;
 import android.widget.Button;
 import android.widget.TextView;
+import android.widget.Toast;
 
+import com.facebook.AccessToken;
+import com.facebook.CallbackManager;
+import com.facebook.FacebookCallback;
+import com.facebook.FacebookException;
+import com.facebook.FacebookSdk;
+import com.facebook.login.LoginManager;
+import com.facebook.login.LoginResult;
+import com.google.android.gms.auth.api.signin.GoogleSignIn;
+import com.google.android.gms.auth.api.signin.GoogleSignInAccount;
+import com.google.android.gms.auth.api.signin.GoogleSignInClient;
+import com.google.android.gms.auth.api.signin.GoogleSignInOptions;
+import com.google.android.gms.common.api.ApiException;
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.Task;
+import com.google.firebase.auth.AuthCredential;
+import com.google.firebase.auth.AuthResult;
+import com.google.firebase.auth.FacebookAuthProvider;
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.FirebaseUser;
+import com.google.firebase.auth.GoogleAuthProvider;
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
+import com.poly.duantotnghiep_jf.MainActivity;
 import com.poly.duantotnghiep_jf.R;
 
+import java.util.Arrays;
+
 public class TaikhoanJob extends AppCompatActivity {
-    TextView btnLoginBasic;
+
+    TextView btnLoginBasic, btnwitGG, btnwithFace;
+
+    GoogleSignInOptions gso;
+    GoogleSignInClient gsc;
+
+    private CallbackManager manager;
+
+    private FirebaseAuth mAuth;
+
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_taikhoan_job);
 
         btnLoginBasic = findViewById(R.id.btn_login_basic);
-
+        btnwitGG = findViewById(R.id.btn_login_withGG);
+        btnwithFace = findViewById(R.id.btn_login_withFB);
         btnLoginBasic.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
@@ -26,7 +68,6 @@ public class TaikhoanJob extends AppCompatActivity {
                 startActivity(intent);
             }
         });
-
-
     }
+
 }
---
+++ app/src/main/java/com/poly/duantotnghiep_jf/Model/Account.java
@@ -0,0 +1,21 @@
+package com.poly.duantotnghiep_jf.Model;
+
+public class Account {
+    public String email;
+    public String name;
+    public String ho;
+    public String phone;
+
+
+
+    public Account() {
+    }
+
+    public Account(String email, String name, String ho, String phone) {
+        this.email = email;
+        this.name = name;
+        this.ho = ho;
+        this.phone = phone;
+    }
+
+}
