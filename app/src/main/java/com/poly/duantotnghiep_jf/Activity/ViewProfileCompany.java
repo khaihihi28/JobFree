de7512b51d6663f03bf988d36de9b3d057d060b9
Bhs2692003 <127473415+Bhs2692003@users.noreply.github.com>
2023-12-03 14:15:05 +0700

    Done Giao diện danh sách công ty, danh sách bài đăng của công ty

---
+++ app/src/main/java/com/poly/duantotnghiep_jf/Activity/ViewProfileCompany.java
@@ -0,0 +1,16 @@
+package com.poly.duantotnghiep_jf.Activity;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+import com.poly.duantotnghiep_jf.R;
+
+public class ViewProfileCompany extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_view_profile_company);
+    }
+}
