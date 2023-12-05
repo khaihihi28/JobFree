54e408a2ebc6edf85d8e292208dda7c4094860cf
khaihihi28 <khaihihi28@gmail.com>
2023-12-06 06:43:58 +0700

    ...

---
+++ app/src/main/java/com/poly/duantotnghiep_jf/Activity/MakeCV.java
@@ -0,0 +1,17 @@
+package com.poly.duantotnghiep_jf.Activity;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+import com.poly.duantotnghiep_jf.R;
+
+public class MakeCV extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_make_cv);
+
+    }
+}
--- app/src/main/java/com/poly/duantotnghiep_jf/Helper/AccountHelper.java
+++ app/src/main/java/com/poly/duantotnghiep_jf/Helper/AccountHelper.java
@@ -16,26 +16,91 @@ public class AccountHelper {
         accountRef.addListenerForSingleValueEvent(valueEventListener);
     }
 
-    public static void checkIsManegeCompany(AccountHelper.OnIsManegeCompanyCheckListener listener) {
-        DatabaseReference manegeCompanyRef = FirebaseDatabase.getInstance().getReference("Account").child(FireBaseHelper.getCurrentUserUid()).child("manegeCompany");
+    public static void checkIsActiveAccount(AccountHelper.OnIsActiveAccountCheckListener listener) {
+        DatabaseReference manegeCompanyRef = FirebaseDatabase.getInstance().getReference("Account").child(FireBaseHelper.getCurrentUserUid()).child("activeAccount");
         manegeCompanyRef.addListenerForSingleValueEvent(new ValueEventListener() {
             @Override
             public void onDataChange(@NonNull DataSnapshot snapshot) {
-                boolean manegeCompany = false;
+                boolean isActive = false;
                 if (snapshot.exists() && snapshot.getValue() instanceof Boolean) {
-                    manegeCompany = (boolean) snapshot.getValue();
+                    isActive = (boolean) snapshot.getValue();
                 }
-                listener.onIsManegeCompanyCheck(manegeCompany);
+                listener.onIsActiveAccountCheck(isActive);
             }
 
             @Override
             public void onCancelled(@NonNull DatabaseError error) {
-                listener.onIsManegeCompanyCheck(false);
+                listener.onIsActiveAccountCheck(false);
             }
         });
     }
     // Interface để trả kết quả kiểm tra về
-    public interface OnIsManegeCompanyCheckListener {
-        void onIsManegeCompanyCheck(boolean manegeCompany);
+    public interface OnIsActiveAccountCheckListener {
+        void onIsActiveAccountCheck(boolean isActive);
+    }
+
+    public static void checkIsManageCompany(AccountHelper.OnIsManageCompanyCheckListener listener) {
+        DatabaseReference manegeCompanyRef = FirebaseDatabase.getInstance().getReference("Account").child(FireBaseHelper.getCurrentUserUid()).child("manegeCompany");
+        manegeCompanyRef.addListenerForSingleValueEvent(new ValueEventListener() {
+            @Override
+            public void onDataChange(@NonNull DataSnapshot snapshot) {
+                boolean isManageCompany = false;
+                if (snapshot.exists() && snapshot.getValue() instanceof Boolean) {
+                    isManageCompany = (boolean) snapshot.getValue();
+                }
+                listener.onIsManageCompanyCheck(isManageCompany);
+            }
+
+            @Override
+            public void onCancelled(@NonNull DatabaseError error) {
+                listener.onIsManageCompanyCheck(false);
+            }
+        });
+    }
+    // Interface để trả kết quả kiểm tra về
+    public interface OnIsManageCompanyCheckListener {
+        void onIsManageCompanyCheck(boolean isManageCompany);
+    }
+
+
+    //Coin
+    public interface CoinCallback {
+        void onCoinReceived(Long coin);
+
+        void onError(String errorMessage);
+    }
+
+    public static void getCoinForAccount(String accountId, final CoinCallback callback) {
+        // Khởi tạo Firebase Database
+        FirebaseDatabase database = FirebaseDatabase.getInstance();
+
+        // Tham chiếu đến node "Account" trong cơ sở dữ liệu
+        DatabaseReference accountRef = database.getReference("Account");
+
+        // Thực hiện truy vấn để lấy dữ liệu của tài khoản cụ thể
+        accountRef.child(accountId).addListenerForSingleValueEvent(new ValueEventListener() {
+            @Override
+            public void onDataChange(@NonNull DataSnapshot dataSnapshot) {
+                // Kiểm tra xem có dữ liệu hay không
+                if (dataSnapshot.exists()) {
+                    // Lấy giá trị của thuộc tính "coin"
+                    Long coin = dataSnapshot.child("coin").getValue(Long.class);
+
+                    // Kiểm tra xem coin có tồn tại hay không
+                    if (coin != null) {
+                        callback.onCoinReceived(coin);
+                    } else {
+                        callback.onError("Không tìm thấy thông tin coin cho tài khoản " + accountId);
+                    }
+                } else {
+                    callback.onError("Không tìm thấy tài khoản có ID " + accountId);
+                }
+            }
+
+            @Override
+            public void onCancelled(@NonNull DatabaseError databaseError) {
+                callback.onError("Lỗi khi đọc dữ liệu từ Firebase: " + databaseError.getMessage());
+            }
+        });
     }
 }
