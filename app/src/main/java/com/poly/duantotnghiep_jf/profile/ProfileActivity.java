5b9f43119065e1b3eec276be2733c838b2048660
Bhs2692003 <127473415+Bhs2692003@users.noreply.github.com>
2024-01-11 18:58:06 +0700

    ProfileActivity , ProfileFragment

---
+++ app/src/main/java/com/poly/duantotnghiep_jf/profile/ProfileActivity.java
@@ -0,0 +1,16 @@
+package com.poly.duantotnghiep_jf.profile;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+import com.poly.duantotnghiep_jf.R;
+
+public class ProfileActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_profile);
+    }
+}
